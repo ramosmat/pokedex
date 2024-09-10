9624f36a1fa0922f8d246802dca2794cd5b16a06
Matheus <matheusramosbi.13@gmail.com>
2024-09-10 16:35:52 -0300

    useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import useFetch from './hooks/useFetch';
 
 function App() {
+  const url = 'https://pokeapi.co/api/v2/pokemon/';
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    async function getPokemons() {
+      const { response, json } = await request(url);
+    }
+
+    getPokemons();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="grid-container">
       <Header />
